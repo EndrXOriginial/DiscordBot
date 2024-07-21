6cfa0ed411054715856d04d11ef835123bcc7a31
EndrXOriginial <chris.pin@hotmail.ca>
2024-07-20 23:36:11 -0400

    removed a console.log

--- events/guildMemeberAdd.js
+++ events/guildMemeberAdd.js
@@ -7,8 +7,6 @@ module.exports = {
         const channel = member.guild.channels.cache.find(channel => channel.name === 'newcomers');
         if (!channel) return;
 
-        console.log(member);
-
         channel.send(`Welcome to The Server ${member.user.username}!\n\nNow probe me, touch me, experiment on me so you can get to know me better.`);
     }
 }
