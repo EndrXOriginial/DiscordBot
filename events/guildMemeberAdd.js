f1cd6690e811266dfbce599f620b7ac853a3f52b
EndrXOriginial <chris.pin@hotmail.ca>
2024-07-21 18:09:07 -0400

    made welcome message Embeded

--- events/guildMemeberAdd.js
+++ events/guildMemeberAdd.js
@@ -1,4 +1,4 @@
-const { Events } = require("discord.js");
+const { Events, EmbedBuilder } = require("discord.js");
 
 module.exports = {
     category: 'events',
@@ -7,6 +7,20 @@ module.exports = {
         const channel = member.guild.channels.cache.find(channel => channel.name === 'newcomers');
         if (!channel) return;
 
-        channel.send(`Welcome to The Server ${member.user.username}!\n\nNow probe me, touch me, experiment on me so you can get to know me better.`);
+        // console.log(member.displayAvatarURL());
+        
+        const welcomeEmbed = new EmbedBuilder()
+        .setColor('F14D4D')
+        .setTitle(`Welcome ${member.user.username}`)
+        .setThumbnail(`${member.displayAvatarURL()}`)
+        .setDescription('Hope you enjoy your stay!')
+        .addFields(
+            {name : '\u200B', value: '\u200B'},
+            {name: 'Rules', value: 'will be button', inline: true},
+            {name: 'Tell us about yourself', value: 'React to this message with the emoji that corresponds to you', inline: true}
+        )
+
+        channel.send({embeds: [welcomeEmbed]});
+        
     }
 }
