2f1942e178d275dc6aea331ece8bfd1ec29de307
EndrXOriginial <chris.pin@hotmail.ca>
2024-07-31 21:56:47 -0400

    started  doing the code for the # guessing game

--- commands/game/numberGuessing.js
+++ commands/game/numberGuessing.js
@@ -4,10 +4,28 @@ const { category } = require("../commandControl/resetCommand");
 
 const numberGuessingCommand = new SlashCommandBuilder()
     .setName('numberguessing')
-    .setDescription('A game where you need to guess the right number between 0 and 50');
+    .setDescription('A game where you need to guess the right number between 0 and 50')
+    .addNumberOptions(option =>
+        option.setName('range')
+            .setDescription('The range in which the secret number can be located')
+            .setRequired(true)
+            .addChoices(
+                {name: '10', value: 10},
+                {name: '50', value: 50},
+                {name: '100', value: 100},
+            )
+    )
 
 const execute = async (interaction) => {
-    await interaction.reply(`You have guessed this number: 00`);
+    if (interaction.channel.name !== 'bot-commands') {
+        interaction.reply({content: 'Please use the command in the "bot-commands" channels. Thank you!', ephemeral: true});
+        return;
+    }
+
+    const numRange = interaction.options.getNumber('range');
+    const secretNumber = Math.floor(Math.random() * numRange);
+
+    interaction.reply(`Choose a number between 0 and ${numRange}`);
 }
 
 module.exports = {
